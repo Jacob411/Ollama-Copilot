03e56efe0ce73e8e08082b40c1bc38aa8a66010b
Jacob Simmons <jakerstx@gmail.com>
2024-07-05 18:32:11 -0500

    added in lua file to handle ghost text and pasting with nvim

---
+++ demo.lua
@@ -0,0 +1,41 @@
+-- demo.lua
+vim.cmd [[highlight MySuggestion guifg=#757575 ctermfg=8]]
+
+local bnr = vim.fn.bufnr('%')
+ns_id = vim.api.nvim_create_namespace('demo')
+
+local line_num = 1
+local col_num = 0
+
+local opts = {
+  end_line = 10,
+  id = 1,
+  virt_text = {{"Here's the first line, should be placed", "MySuggestion"}},
+  virt_lines = {
+    {{"Here's come virtual text adding in", "MySuggestion"}},
+    {{"SECOND LINE", "IncSearch"}}
+  },
+  virt_text_pos = 'inline',
+  -- virt_text_win_col = 20,
+}
+function add_extmark()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  line_num = cursor[1]
+  col_num = cursor[2]
+  print(line_num, col_num)
+  mark_id = vim.api.nvim_buf_set_extmark(0, ns_id, line_num - 1, col_num, opts)
+end
+
+function delete_extmark()
+  local nsId = vim.api.nvim_get_namespaces()["demo"]
+  vim.api.nvim_buf_del_extmark(0, nsId, 1)
+end
+
+function paste_extmark()
+  local data = 'Here is the data'
+  vim.api.nvim_paste(data, true, -1)
+end
+
+vim.api.nvim_set_keymap('n', '<leader>dm', '<cmd>lua delete_extmark()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>da', '<cmd>lua add_extmark()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dp', '<cmd>lua paste_extmark()<CR>', { noremap = true, silent = true })
