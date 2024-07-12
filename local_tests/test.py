22bfce791de9a8fea3a3a39f5517a5039fe94d75
Jacob Simmons <jakerstx@gmail.com>
2024-07-12 18:15:32 -0500

    restructure files. Fit a more nvim plugin format

--- local_tests/test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-from transformers import pipeline
-
-print(torch.__version__)
-print(torchvision.__version__)
-sentiment_pipeline = pipeline('sentiment-analysis')
-result = sentiment_pipeline('We love you')
-print(result)
