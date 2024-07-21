4e0e742aa74f2d46174b95d5bf8b7679d1307985
Jacob Simmons <jakerstx@gmail.com>
2024-07-20 23:34:34 -0500

    update to not call set up prematurely

--- plugin/ollama-copilot.lua
+++ plugin/ollama-copilot.lua
@@ -1,8 +1,4 @@
-require('OllamaCopilot').setup({
-  -- Your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-})
+require('OllamaCopilot')
 --
 -- -- Default configuration
 -- local default_config = {
